e5a85afc61605d1113ab4222aa34d1957af7fa36
Zelnonmon <69782394+Zelnonmon045@users.noreply.github.com>
2022-08-25 16:45:03 +1000

    Renamed files and variables.

--- Assets/Scripts/Player/PlayerVirusController.cs
+++ Assets/Scripts/Player/PlayerVirusController.cs
@@ -1,11 +1,11 @@
-using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerVirusController : MonoBehaviour
 {
-    [SerializeField] private float MovementSpeed = 0.5f;
-    [SerializeField] private Transform CameraAnchor;
+    [SerializeField] private float movementSpeed = 10f;
+    [SerializeField] private float cameraSensitivity = 0.5f;
+    [SerializeField] private Transform cameraAnchor;
     private Rigidbody rb;
     private InputAction moveAction;
 
@@ -40,17 +40,21 @@ public class PlayerVirusController : MonoBehaviour
     {
         //Unregister input events from InputManager
         InputManager inputManager = GameManager.InputManager;
+        if (!inputManager)
+        {
+            return;
+        }
         inputManager.GetAction(InputManager.Controls.Look).performed -= RotateCamera;
         inputManager.GetAction(InputManager.Controls.Move).canceled -= MovementHalt;
     }
 
     private void RotateCamera(InputAction.CallbackContext LookDelta)
     {
-        Vector2 lookDeltaV2 = LookDelta.ReadValue<Vector2>();
-        Vector2 rot = CameraAnchor.eulerAngles;
+        Vector2 lookDeltaV2 = LookDelta.ReadValue<Vector2>() * cameraSensitivity;
+        Vector2 rot = cameraAnchor.eulerAngles;
         rot.x = Mathf.Clamp(rot.x - lookDeltaV2.y - (rot.x > 90 ? 360 : 0), -90, 90);
         rot.y += lookDeltaV2.x;
-        CameraAnchor.eulerAngles = rot;
+        cameraAnchor.eulerAngles = rot;
     }
 
     //Updates movement direction according to input recieved
@@ -58,10 +62,10 @@ public class PlayerVirusController : MonoBehaviour
     private void Move()
     {
         Vector2 moveDeltaV2 = moveAction.ReadValue<Vector2>();
-        Vector3 xAxis = CameraAnchor.right;
+        Vector3 xAxis = cameraAnchor.right;
         Vector3 forward = Vector3.Cross(xAxis, Vector3.up);
         Vector3 direction = (xAxis * moveDeltaV2.x) + (forward * moveDeltaV2.y);
-        rb.velocity = direction * MovementSpeed;
+        rb.velocity = direction * movementSpeed;
     }
 
     //Sets movement to zero when input is removed
