748de81b57f383675ab6d94128b175c081db8d9f
YuCheng <69775014+Yu-C-UTS@users.noreply.github.com>
2022-09-03 20:05:43 +0800

    Made a cell tower thing for the player to interact and exit

---
+++ Assets/Scripts/EnvironmentObjects/PlayerExit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerExit : MonoBehaviour
+{
+    public void ExitLevel()
+    {
+        Debug.Log("Player Exit.");
+    }
+}
