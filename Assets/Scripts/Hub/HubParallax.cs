b64b7b870eeb8f9af0c42eaf6aa570f8e7ef4567
Zelnonmon <69782394+Zelnonmon045@users.noreply.github.com>
2022-09-12 11:34:59 +1000

    Added settings icon

--- Assets/Scripts/Hub/HubParallax.cs
+++ Assets/Scripts/Hub/HubParallax.cs
@@ -6,18 +6,27 @@ public class HubParallax : MonoBehaviour
 
     private RectTransform rect;
     private Vector2 originalPos;
+    private Vector3 originalOffset;
+
+    [SerializeField] private bool useCustomPivotPoint = false;
+    [Tooltip("(0, 0) BottomLeft, (1, 1) TopRight"), SerializeField]
+    private Vector2 customPivotPoint;
+
+
     private Camera cam;
-    
+
     void Start()
     {
         rect = (RectTransform)transform;
         originalPos = rect.localPosition;
+
         cam = Camera.main;
+        originalOffset = useCustomPivotPoint ? customPivotPoint : cam.ScreenToViewportPoint(transform.position);
     }
 
     void Update()
     {
-        Vector2 mousePos = cam.ScreenToViewportPoint(Input.mousePosition) - new Vector3(0.5f, 0.5f);
+        Vector2 mousePos = cam.ScreenToViewportPoint(Input.mousePosition) - originalOffset;
         rect.localPosition = new()
         {
             x = originalPos.x + (mousePos.x * parallaxStrength),
