473a9aaaed5d9ef904bfda757b7af395cd0dcecd
Zelnonmon <69782394+Zelnonmon045@users.noreply.github.com>
2022-09-15 10:50:09 +1000

    Got Application windows working.

--- Assets/Scripts/UI/DesktopWindowTitleBarBehaviour.cs
+++ Assets/Scripts/UI/DesktopWindowTitleBarBehaviour.cs
@@ -10,6 +10,7 @@ public class DesktopWindowTitleBarBehaviour : MonoBehaviour, IPointerEnterHandle
     private InputManager input;
     private bool isHovering;
     private bool isPressed = false;
+    private Vector2 windowDistance;
 
     private void Start()
     {
@@ -19,7 +20,6 @@ public class DesktopWindowTitleBarBehaviour : MonoBehaviour, IPointerEnterHandle
         {
             new(Hub, input.GetAction(Click), Performed, OnHoverClick),
             new(Hub, input.GetAction(Click), Canceled, OnHoverUnclick),
-            new(Hub, input.GetAction(Move), Started, OnFocusStartMove),
             new(Hub, input.GetAction(Move), Performed, OnFocusMove),
         });
     }
@@ -37,6 +37,13 @@ public class DesktopWindowTitleBarBehaviour : MonoBehaviour, IPointerEnterHandle
     private void OnHoverClick(InputAction.CallbackContext moveDelta)
     {
         isPressed = isHovering;
+        if (isPressed)
+        {
+            RectTransform rectTransform = (RectTransform)transform.root;
+            Vector2 half = new(rectTransform.rect.width * 0.5f, rectTransform.rect.height * 0.5f);
+            Vector2 mouseStartPosition = rectTransform.rect.size * Camera.main.ScreenToViewportPoint(Input.mousePosition) - half;
+            windowDistance = mouseStartPosition - (Vector2)transform.root.InverseTransformPoint(transform.parent.position);
+        }
     }
 
     private void OnHoverUnclick(InputAction.CallbackContext moveDelta)
@@ -44,16 +51,14 @@ public class DesktopWindowTitleBarBehaviour : MonoBehaviour, IPointerEnterHandle
         isPressed = false;
     }
 
-    private void OnFocusStartMove(InputAction.CallbackContext moveDelta)
-    {
-
-    }
-
     private void OnFocusMove(InputAction.CallbackContext moveDelta)
     {
         if (isPressed)
         {
-            transform.parent.Translate(moveDelta.ReadValue<Vector2>());
+            RectTransform rectTransform = (RectTransform)transform.root;
+            Vector2 half = new(rectTransform.rect.width * 0.5f, rectTransform.rect.height * 0.5f);
+            Vector2 mousePosition = rectTransform.rect.size * Camera.main.ScreenToViewportPoint(Input.mousePosition) - half;
+            ((RectTransform)transform.parent).anchoredPosition = mousePosition - windowDistance;
         }
     }
 }
