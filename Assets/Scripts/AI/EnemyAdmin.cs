63fea78c780bda994f5dc6328b439a1cbc95e420
YuCheng <69775014+Yu-C-UTS@users.noreply.github.com>
2022-09-21 00:51:53 +0800

    Gave EnemyAdmin the ability to control light color around the world

--- Assets/Scripts/AI/EnemyAdmin.cs
+++ Assets/Scripts/AI/EnemyAdmin.cs
@@ -6,6 +6,21 @@ public class EnemyAdmin : MonoBehaviour
 {
     [SerializeField] private AINode[] allAiNodes;
 
+    [Header("Alert Status Light Color")]
+    [SerializeField, Range(0, 1)] private float alertness = 0;
+    [SerializeField, GradientUsage(true, ColorSpace.Linear)] private Gradient worldAlertLightColor;
+    [SerializeField, ColorUsage(false, true)] private Color fullAlertColor;
+    [SerializeField] private float fullAlertColorLerpTime = 2;
+    private float fullAlertColorLerp = 0;
+
+
+    private void Update()
+    {
+        //if alertness is basically 1, lerp into full alert color, otherwise evaluate the gradient
+        fullAlertColorLerp = Mathf.Clamp( fullAlertColorLerp + Time.deltaTime * (Mathf.Approximately(alertness, 1) ? 1 : -1), 0, fullAlertColorLerpTime);
+        Shader.SetGlobalColor("_StatusEmissionColor", Color.Lerp( worldAlertLightColor.Evaluate(alertness), fullAlertColor, fullAlertColorLerp / fullAlertColorLerpTime));
+    }
+
     public void NewAiNodes(AINode[] newAiNodes)
     {
         allAiNodes = newAiNodes;
