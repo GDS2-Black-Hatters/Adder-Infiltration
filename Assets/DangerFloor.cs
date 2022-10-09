c630aeabee9b68419dcd489c46dcf0e67c99798f
JSapier <39616698+JSapier@users.noreply.github.com>
2022-10-09 21:59:12 +1100

    Added icons for fishing minigame + fixed final boss level a bit

---
+++ Assets/DangerFloor.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerFloor : MonoBehaviour
+{
+    // On trigger enter, damage player (and slow?)
+    // On trigger exit, stop doing that
+}
