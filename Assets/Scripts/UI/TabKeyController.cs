9da490e0d52596d895ad68e2b2a69442f0e5b5b2
Quang Nhat Le <65646407+tahnel@users.noreply.github.com>
2022-10-21 19:47:14 +1100

    2 Tutorials refined

--- Assets/Scripts/UI/TabKeyController.cs
+++ Assets/Scripts/UI/TabKeyController.cs
@@ -15,32 +15,32 @@ public class TabKeyController : MonoBehaviour
         gameObject.AddComponent<ActionInputSubscriber>().AddActions(new()
         {
             new(MainGameTab, Performed, Hold),
-            new(MainGameTab, Canceled, Release)
+            //new(MainGameTab, Canceled, Release)
         });
+
+        Shader.SetGlobalFloat("_QuestIconMaxAlpha", 1);
     }
     private void Hold(InputAction.CallbackContext callbackContext)
     {
-        //Set enabled to both objective list and icons
-        if(!isActive)
-        {
-            isActive = true;
-            objectiveList.SetActive(isActive);
-            transparentBG.SetActive(isActive);
-            //Enable Icons when Release Tab
-            //Icons.SetActive(isActive);
-        }
+        
+        isActive = !isActive;
+        objectiveList.SetActive(isActive);
+        transparentBG.SetActive(isActive);
+        Shader.SetGlobalFloat("_QuestIconMaxAlpha",isActive? 2:0);
+        //Enable Icons when Release Tab
+        //Icons.SetActive(isActive);
     } 
 
-    private void Release(InputAction.CallbackContext callbackContext)
-    {
-        //Set disabled to both objective list and icons
-        if(isActive)
-        {
-            isActive = false;
-            objectiveList.SetActive(isActive);
-            transparentBG.SetActive(isActive);
-            //Disable Icons when Release Tab
-            //Icons.SetActive(isActive);
-        }
-    }
+    // private void Release(InputAction.CallbackContext callbackContext)
+    // {
+    //     //Set disabled to both objective list and icons
+    //     if(isActive)
+    //     {
+    //         isActive = false;
+    //         objectiveList.SetActive(isActive);
+    //         transparentBG.SetActive(isActive);
+    //         //Disable Icons when Release Tab
+    //         //Icons.SetActive(isActive);
+    //     }
+    // }
 }
