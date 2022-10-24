c3a01514520499ac1129afd3d9aaf3b99d7907b5
Oscar <oscar.s.browne@student.uts.edu.au>
2022-10-24 17:23:25 +1100

    Menu music loop

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -20,6 +20,8 @@ namespace AK
         static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID OBJECTIVECOMPLETE_EVENT = 551763010U;
         static const AkUniqueID PLAYERDISCOVERED_EVENT = 2538572501U;
+        static const AkUniqueID TARGETHOOKED = 2827905996U;
+        static const AkUniqueID TARGETSTASHED = 2045263066U;
     } // namespace EVENTS
 
     namespace STATES
