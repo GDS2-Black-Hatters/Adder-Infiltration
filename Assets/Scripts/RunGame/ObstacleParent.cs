aa7ae2be53be30ab3176938196c8d4b10f93fea4
MaxGottardi <13959073@student.uts.edu.au>
2022-10-24 15:21:58 +1100

    Made the runner collision work through engine physics
    the runner now physically collides with the obstacle boxes, am working on actual lose state

--- Assets/Scripts/RunGame/ObstacleParent.cs
+++ Assets/Scripts/RunGame/ObstacleParent.cs
@@ -18,7 +18,7 @@ public class ObstacleParent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        obstacle = GameObject.Find("Obstacle").GetComponent<Rigidbody>();
+        obstacle = gameObject.GetComponentInChildren<Rigidbody>();
         maxTimer = fullTimer;
         timer = fullTimer;
     }
@@ -39,23 +39,34 @@ public class ObstacleParent : MonoBehaviour
 
         if (timer <= 0)
         {
-            Rigidbody obstacleRb;
-            Vector3 spawnPoint = new Vector3((Random.Range(-9, 9)), Random.Range(0, 3), gameObject.transform.position.z);
-            obstacleRb = Instantiate(obstacle, spawnPoint, gameObject.transform.rotation);
-            obstacleRb.velocity = new Vector3(0, 0, -10 / CalculateTimer());
-            //obstacleRb.GetComponent<Collider>().isTrigger = false;
-            
-            obstacleRb.GetComponent<Transform>().localScale = new Vector3(Random.Range(2,5), Random.Range(2, 5),1);
+            SpawnObstacle();
             if (maxTimer > 1)
             {
                 maxTimer -= 0.5f;
             }
             timer = maxTimer;
         }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SpawnObstacle();
+        }
     }
 
     float CalculateTimer()
     {
         return maxTimer / fullTimer;
+    }
+
+    void SpawnObstacle()
+    {
+        Rigidbody obstacleRb;
+        Vector3 spawnPoint = new Vector3((Random.Range(-9, 9)), Random.Range(0, 3), gameObject.transform.position.z);
+        obstacleRb = Instantiate(obstacle, spawnPoint, gameObject.transform.rotation);
+
+        obstacleRb.GetComponent<Collider>().isTrigger = false;
+        obstacleRb.GetComponent<Transform>().localScale = new Vector3(Random.Range(2, 5), Random.Range(2, 5), 1);
+        obstacleRb.GetComponent<Transform>().position = new Vector3(obstacleRb.GetComponent<Transform>().position.x, (obstacleRb.GetComponent<Transform>().localScale.y / 2) + 1, obstacleRb.GetComponent<Transform>().position.z);
+        obstacleRb.velocity = new Vector3(0, 0, -10 / CalculateTimer());
     }
 }
